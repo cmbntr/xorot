cd020745423c5970c1ebc751681b6bb0e7b49d3d
Michael Locher <cmbntr@gmail.com>
2024-01-27 17:35:45 +0100

    initial

---
+++ src/main.zig
@@ -0,0 +1,35 @@
+const std = @import("std");
+const io = std.io;
+
+pub fn main() !void {
+    const stdin = io.getStdIn().reader();
+    const stdout = io.getStdOut().writer();
+    try xorot(stdin, stdout);
+}
+
+fn xorot(in: std.fs.File.Reader, out: std.fs.File.Writer) !void {
+    var idx: u8 = 0;
+    var buffer: [8192]u8 = undefined;
+
+    while (true) {
+        const cnt = try in.read(&buffer);
+        if (cnt == 0) break;
+
+        for (0..cnt) |i| {
+            buffer[i] = rot(rot(buffer[i]) ^ idx);
+            idx = if (idx == 0xFF) 0 else idx + 1;
+        }
+
+        try out.writeAll(buffer[0..cnt]);
+    }
+}
+
+inline fn rot(ch: u8) u8 {
+    return switch (ch) {
+        'A'...'M', 'a'...'m' => ch + 13,
+        'N'...'Z', 'n'...'z' => ch - 13,
+        '0'...'4' => ch + 5,
+        '5'...'9' => ch - 5,
+        else => ch,
+    };
+}
